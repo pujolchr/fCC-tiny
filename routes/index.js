192f44c6519ff8ab1d916971a45775a0629c3fe3
Christian Pujol <pujolchr@gmail.com>
2017-09-18 00:06:40 +0200

    form to the landing page added

--- routes/index.js
+++ routes/index.js
@@ -8,13 +8,22 @@ router.get('/', (req, res, next) => {
   // yes display the data
   // no display just the form
   const source = req.query.url;
- 
-  res.render('index', {
+
+  const obj = {
     title: 'fCC-tiny',
     author: 'Christan Pujol',
     url: source,
-    tiny: `${req.host}/tiny/1`,
-  });
+  };
+  if (source) {
+    // get the tiny form
+    obj.tiny = `${req.hostname}/1/${source}`;
+  }
+  res.render('index', obj);
 });
+
+router.get('/:tiny', (req, res, next) => {
+  res.send('redirect');
+});
+
 
 module.exports = router;
